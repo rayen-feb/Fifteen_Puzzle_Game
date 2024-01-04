505c3d57a26044238c175f72cddc38b99e0a76ea
AHMED RAYEN <131598929+rayen-feb@users.noreply.github.com>
2024-01-04 09:41:14 -1100

    Update main.cpp

--- 06 Fifteen-Puzzle/main.cpp
+++ 06 Fifteen-Puzzle/main.cpp
@@ -1,1 +1,12 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
 
+using namespace sf;
+
+int main()
+{
+    RenderWindow app(VideoMode(256,256), "15-Puzzle!");
+	app.setFramerateLimit(60);
+
+	Texture t;
+    t.loadFromFile("images/15.png");
